36c06d35eaad88b00f1a8d0476faebf7c796822b
Rafael Baron <rafael@shocklogic.com>
2018-11-27 11:32:52 -0400

    Adding icons in the header

--- app/components/Header/MainMenuHeader.js
+++ app/components/Header/MainMenuHeader.js
@@ -5,9 +5,9 @@ import styles from './styles';
 const MainMenuHeader = () => (
   <Header
     backgroundColor={styles.header.backgroundColor}
-    leftComponent={{ icon: 'menu', color: 'white' }}
+    leftComponent={{ icon: 'keyboard-arrow-left', color: 'white', margin: -3 }}
     centerComponent={{ text: 'Main Menu', style: { color: 'white' } }}
-    rightComponent={{ icon: 'home', color: 'white' }}
+    rightComponent={{ icon: 'notifications', color: 'white', margin: -3 }}
   />
 );
 
